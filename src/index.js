e5febfde8a9366b0f988e83b2923799032876dd9
Anton <antonmihail@gmail.com>
2023-03-14 10:53:10 -0400

    added semantic ui

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import "./assets/vendor/fullPage.js/dist/jquery.fullpage.min.css"
-import "./assets/vendor/slick/slick.min.css"
-import "./assets/vendor/vegas/vegas.min.css"
-import "./assets/vendor/magnific-popup/dist/magnific-popup.min.css"
-import "./assets/vendor/animate.css/animate.min.css"
-import "./assets/vendor/font-awesome/css/all.min.css"
-import "./assets/vendor/themify-icons/css/themify-icons.css"
-import "./assets/css/theme.css"
-import "./assets/css/custom.css"
+import 'semantic-ui-css/semantic.min.css'
+import "./styles/semanic.flatly.css";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
