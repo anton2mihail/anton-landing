0f37c4bf55fe2995121f3e00b4f615fc49d57611
Anton <antonmihail@gmail.com>
2023-03-19 17:09:12 -0400

    Restart

--- src/components/Navbar/Navbar.styes.jsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-const StyledNavbar = styled.div`
-  grid-area: content;
-`;
-
-// eslint-disable-next-line import/prefer-default-export
-export default StyledNavbar;
--- src/components/TopLogo/TopLogo.styles.jsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-const StyledTopLogo = styled.div`
-  grid-area: logo;
-`;
-
-// eslint-disable-next-line import/prefer-default-export
-export default StyledTopLogo;
