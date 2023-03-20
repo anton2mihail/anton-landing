9df2f65e32b15293199d25f74dc6a423c6ae225d
Anton <antonmihail@gmail.com>
2023-03-19 20:56:43 -0400

    final updates for the day

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -14,18 +14,26 @@ import {
   Avatar,
 } from '@chakra-ui/react';
 import { BsLinkedin, BsGithub } from 'react-icons/bs';
+import { Link } from '@chakra-ui/next-js';
 
 function Intro({}: IntroProps) {
   return (
     <div className={styles.introContainer}>
       <div className={styles.introText}>
-        <Heading size="xl">
+        <Heading size="2xl">
           {' '}
           Hello, I’m Toni, a Software Engineer With 5+ years of experience.
         </Heading>
-        <Text>View a summary of all your customers over the last month.</Text>
+        <Text mt={5}>
+          As an experienced software engineer, I am passionate about writing
+          clean and efficient code, collaborating with others to deliver
+          high-quality software, and thrive under pressure when taking on new
+          challenges.
+        </Text>
         <div>
           <Button
+            as={Link}
+            href={'/'}
             mr={1}
             style={{
               background: '#1D1D1D',
@@ -39,8 +47,29 @@ function Intro({}: IntroProps) {
             }}>
             Contact Me
           </Button>
-          <Avatar mr={2} bg="linkedin.400" icon={<Icon as={BsLinkedin} />} />
-          <Avatar bg="gray.400" icon={<Icon as={BsGithub} />} />
+          <Avatar
+            as={Link}
+            target="_blank"
+            href="https://north-boutique-landing-page.onrender.com"
+            mr={2}
+            bg="blue.700"
+            src="/north-boutique-logo.png"
+          />
+          <Avatar
+            as={Link}
+            target="_blank"
+            href="https://www.linkedin.com/in/toni-lachmaniucu/"
+            mr={2}
+            bg="linkedin.400"
+            icon={<Icon as={BsLinkedin} />}
+          />
+          <Avatar
+            as={Link}
+            target="_blank"
+            href="https://github.com/anton2mihail"
+            bg="gray.400"
+            icon={<Icon as={BsGithub} />}
+          />
         </div>
       </div>
       <div className={styles.introImage}>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,53 +5,50 @@ import handleClickScroll from '@/utilities/scrollIntoView';
 import { NavbarProps } from './Navbar.types';
 
 function Navbar({ activeSection, setActiveSection }: NavbarProps) {
-	const styleByActive = (name: string) => {
-		return activeSection === name ? { color: 'black' } : { color: '#727272' };
-	};
+  const styleByActive = (name: string) => {
+    return activeSection === name ? { color: 'black' } : { color: '#727272' };
+  };
 
-	return (
-		<div className={styles.navbarContainer}>
-			<div className={styles.avatarNavbar}>
-				<Avatar mr={2} size="xs" name="Toni" src="/lightbluecircleicon.png" />{' '}
-				Toni
-			</div>
-			<div className={styles.actionsNavbar}>
-				<Button
-					style={styleByActive('home')}
-					onClick={() => {
-						setActiveSection('home');
-						handleClickScroll('home', 'smooth');
-					}}
-					colorScheme="gray"
-					variant="ghost"
-				>
-					Home
-				</Button>
-				<Button
-					style={styleByActive('projects')}
-					onClick={() => {
-						setActiveSection('projects');
-						handleClickScroll('projects', 'smooth');
-					}}
-					colorScheme="gray"
-					variant="ghost"
-				>
-					Projects
-				</Button>
-				<Button
-					style={styleByActive('contact')}
-					onClick={() => {
-						setActiveSection('contact');
-						handleClickScroll('contact', 'smooth');
-					}}
-					colorScheme="gray"
-					variant="ghost"
-				>
-					Content
-				</Button>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.navbarContainer}>
+      <div className={styles.avatarNavbar}>
+        <Avatar mr={2} size="xs" name="Toni" src="/lightbluecircleicon.png" />{' '}
+        Toni
+      </div>
+      <div className={styles.actionsNavbar}>
+        <Button
+          style={styleByActive('home')}
+          onClick={() => {
+            setActiveSection('home');
+            handleClickScroll('home', 'smooth');
+          }}
+          colorScheme="gray"
+          variant="ghost">
+          Home
+        </Button>
+        <Button
+          style={styleByActive('projects')}
+          onClick={() => {
+            setActiveSection('projects');
+            handleClickScroll('projects', 'smooth');
+          }}
+          colorScheme="gray"
+          variant="ghost">
+          Projects
+        </Button>
+        <Button
+          style={styleByActive('contact')}
+          onClick={() => {
+            setActiveSection('contact');
+            handleClickScroll('contact', 'smooth');
+          }}
+          colorScheme="gray"
+          variant="ghost">
+          Contact
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
